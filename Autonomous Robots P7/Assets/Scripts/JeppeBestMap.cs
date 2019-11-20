ebc9b1011a6528190e3f8c227cf68695e863f5db
EmilKamel <Emil.kloster.lindberg@gmail.com>
2019-11-20 16:05:25 +0100

    Create JeppeBestMap.cs
    Hej

---
+++ Autonomous Robots P7/Assets/Scripts/JeppeBestMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeppeBestMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
+
+public class Vertex : MonoBehaviour
+{
+    //TODO This.pos som 00 vertex
+    //TODO 00 vertex's naboer tilføjes og gøres til frontier
+    //TODO Hvis hit(), så er denne vertex blocked.
+
+
+    public Vector3 pos;
+    public Bool frontier;
+    public Bool blocked;
+
+    public List<Vertex> Neighbors;
+}
+
+public class Matrix : MonoBehaviour
+{
+    //TODO: 2d array List<List<field>>
+    //
+    //Field = pos, points, blocked, frontier. NICE:)
+
+}
+
